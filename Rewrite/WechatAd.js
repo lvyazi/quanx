dddd523934d878278643db6226f706de548c3ec1
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-11 00:04:12 +0800

    Add files via upload

---
+++ Rewrite/WechatAd.js
@@ -0,0 +1,16 @@
+/*
+微信公众号Ad
+QX:
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+Surge4：
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+Surge & QX MITM = mp.weixin.qq.com,
+*/
+
+var obj = JSON.parse($response.body);
+obj.advertisement_num = 0;
+obj.advertisement_info = [];
+delete obj.appid;
+$done({body: JSON.stringify(obj)}); 
