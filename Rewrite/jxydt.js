05e58e4a79e77d0e715974bb408a61a6ce0816ad
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-10 23:30:07 +0800

    Add files via upload

---
+++ Rewrite/jxydt.js
@@ -0,0 +1,29 @@
+/*
+驾校一点通
+^https:\/\/vipapi\.jxedt\.com\/vip\/check url script-response-body https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Script/jxydt.js
+MITM = vipapi.jxedt.com
+
+/*
+
+var obj = JSON.parse($response.body);
+ obj= {
+ "code": 0,
+ "msg": "OK",
+ "result": {
+  "vipstatus": 1,
+  "isexpert": true,
+  "endtime": "2022-11-02",
+  "vipType": 0,
+  "begintime": "2019-11-03",
+  "isRenewals": 1,
+  "vipLevel": 0,
+  "openCourseIds": [],
+  "choreographySkills": 0,
+  "has_append_service": 0,
+  "vipDaysOverdue": -1,
+  "vipResidualDay": 999,
+  "imeiVIPOrderBindStatus": 1
+ }
+};
+$done({body: JSON.stringify(obj)});
+//
