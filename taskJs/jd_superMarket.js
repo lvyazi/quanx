d60c38d73321575b8edc5e4246d98f2185afe712
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-14 18:50:59 +0800

    Update jd_superMarket.js

--- taskJs/jd_superMarket.js
+++ taskJs/jd_superMarket.js
@@ -13,12 +13,12 @@ Some Functions Modified From https://github.com/Zero-S1/JD_tools/blob/master/JD_
 // QuantumultX
 [task_local]
 #东东超市
-11 1-23/5 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_superMarket.js, tag=东东超市, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxc.png, enabled=true
+11 1-23/5 * * * https://raw.githubusercontent.com/lvyazi/quanx/main/taskJs/jd_superMarket.js, tag=东东超市, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jxc.png, enabled=true
 // Loon
 [Script]
-cron "11 1-23/5 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_superMarket.js,tag=东东超市
+cron "11 1-23/5 * * *" script-path=https://raw.githubusercontent.com/lvyazi/quanx/main/taskJs/jd_superMarket.js,tag=东东超市
 // Surge
-东东超市 = type=cron,cronexp="11 1-23/5 * * *",wake-system=1,timeout=320,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_superMarket.js
+东东超市 = type=cron,cronexp="11 1-23/5 * * *",wake-system=1,timeout=320,script-path=https://raw.githubusercontent.com/lvyazi/quanx/main/taskJs/jd_superMarket.js
  */
 const $ = new Env('东东超市');
 //Node.js用户请在jdCookie.js处填写京东ck;
