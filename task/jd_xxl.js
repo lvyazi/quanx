6466865e90481d246b01a0618c9c814a0de511e7
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-18 23:46:19 +0800

    Update jd_xxl.js

--- task/jd_xxl.js
+++ task/jd_xxl.js
@@ -12,17 +12,17 @@
 ============Quantumultx===============
 [task_local]
 #东东爱消除
-0 * * * * https://raw.githubusercontent.com/shylocks/Loon/main/jd_xxl.js, tag=东东爱消除, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/shylocks/jd_xxl.jpg, enabled=true
+0 * * * * https://raw.githubusercontent.com/lvyazi/quanx/main/task/jd_xxl.js, tag=东东爱消除, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/shylocks/jd_xxl.jpg, enabled=true
 
 ================Loon==============
 [Script]
-cron "0 * * * *" script-path=https://raw.githubusercontent.com/shylocks/Loon/main/jd_xxl.js,tag=东东爱消除
+cron "0 * * * *" script-path=https://raw.githubusercontent.com/lvyazi/quanx/main/task/jd_xxl.js,tag=东东爱消除
 
 ===============Surge=================
-东东爱消除 = type=cron,cronexp="0 * * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/shylocks/Loon/main/jd_xxl.js
+东东爱消除 = type=cron,cronexp="0 * * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lvyazi/quanx/main/task/jd_xxl.js
 
 ============小火箭=========
-东东爱消除 = type=cron,script-path=https://raw.githubusercontent.com/shylocks/Loon/main/jd_xxl.js, cronexpr="0 * * * *", timeout=200, enable=true
+东东爱消除 = type=cron,script-path=https://raw.githubusercontent.com/lvyazi/quanx/main/task/jd_xxl.js, cronexpr="0 * * * *", timeout=200, enable=true
  */
 const $ = new Env('东东爱消除');
 const notify = $.isNode() ? require('./sendNotify.js') : '';
