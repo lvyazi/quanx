5b8dbcd106b8a3b4b4969f0513cb893faa8efad1
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-29 20:26:32 +0800

    Update shuyuan.js

--- task/shuyuan.js
+++ task/shuyuan.js
@@ -12,7 +12,7 @@ boxjs IMMORTAL_LATLON
 ============Quantumultx===============
 [task_local]
 #京东神仙书院
-20 8 * * * https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_immortal.js, tag=京东神仙书院, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
+20 8 * * * https://raw.githubusercontent.com/lvyazi/quanx/main/task/shuyuan.js, tag=京东神仙书院, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
 
 ================Loon==============
 [Script]
