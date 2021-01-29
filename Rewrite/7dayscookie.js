a3b91d29836a15d2c6bad0979726308d9291b473
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-29 19:48:42 +0800

    Update 7dayscookie.js

--- Rewrite/7dayscookie.js
+++ Rewrite/7dayscookie.js
@@ -3,7 +3,7 @@
  * [Script]
  *梨涡签到领现金
  *cron "7 0 * * *" script-path=https://raw.githubusercontent.com/iisams/Scripts/master/liwo/7days.js,tag=梨涡签到领现金
- *http-request https:\/\/api\.m\.jd\.com\/api\/v1\/sign\/doSign script-path=https://raw.githubusercontent.com/iisams/Scripts/master/liwo/7dayscookie.js, requires-body=true, timeout=10, tag=梨涡签到领现金Cookie
+ *https:\/\/api\.m\.jd\.com\/api\/v1\/sign\/doSign url script-request-body https://raw.githubusercontent.com/lvyazi/quanx/main/Rewrite/7dayscookie.js
  *
  * [MITM]
  *
