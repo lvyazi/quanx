e55dcc26a42cb63bf9fc1e212a961becaf945008
lvyazi <77226255+lvyazi@users.noreply.github.com>
2021-01-29 20:32:24 +0800

    Update nianshouar.js

--- task/nianshouar.js
+++ task/nianshouar.js
@@ -8,7 +8,7 @@
 ============Quantumultx===============
 [task_local]
 #京东炸年兽AR
-0 9 * * * https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_nian_ar.js, tag=京东炸年兽AR, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/lxk0301/jd_nian.png, enabled=true
+0 9 * * * https://raw.githubusercontent.com/lvyazi/quanx/main/task/nianshouar.js, tag=京东炸年兽AR, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/lxk0301/jd_nian.png, enabled=true
 
 ================Loon==============
 [Script]
